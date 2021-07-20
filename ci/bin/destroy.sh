76047618e97951c9bbaa117bee76119a964ab12a
Ben Latham <benlatham123@hotmail.co.uk>
2021-07-20 15:12:52 +0100

    single pipeline not branches

--- ci/bin/destroy.sh
+++ ci/bin/destroy.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-: ${BRANCH}
 : ${HTTPS_PROXY:="localhost:8118"}
 : ${TARGET:=gcp}
 : ${FLY:=fly -t ${TARGET}}
@@ -11,7 +10,6 @@ set -euo pipefail
 
 export HTTPS_PROXY=${HTTPS_PROXY}
 
-WORKSPACE=`echo "${BRANCH:0:13}" | sed 's/[^a-zA-Z0-9]/-/g' | tr '[:upper:]' '[:lower:]'`
-pipeline="baw-client-rest-${WORKSPACE}"
+pipeline="baw-client-rest"
 
 ${FLY} destroy-pipeline -p ${pipeline} --non-interactive
--- ci/bin/fly_pipeline.sh
+++ ci/bin/fly_pipeline.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-: ${BRANCH}
 : ${HTTPS_PROXY:="localhost:8118"}
 : ${TARGET:=gcp}
 : ${FLY:=fly -t ${TARGET}}
@@ -12,14 +11,12 @@ set -euo pipefail
 export HTTPS_PROXY=${HTTPS_PROXY}
 
 WORKSPACE=`echo "${BRANCH:0:13}" | sed 's/[^a-zA-Z0-9]/-/g' | tr '[:upper:]' '[:lower:]'`
-pipeline="baw-client-rest-${WORKSPACE}"
+pipeline="baw-client-rest"
 
 
 ${FLY} set-pipeline \
     -p ${pipeline} \
     -c ci/pipeline.yml \
-    -v "branch=${BRANCH}" \
-    -v "workspace=${WORKSPACE}" \
     ${EXTRA_OPTIONS}
 
 
